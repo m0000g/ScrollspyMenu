ae5c66d6a5e9c8512f021f3a7c4f78129f5c4509
Maria Gabriela Perez <m0000g@gmail.com>
2016-08-17 16:40:19 -0400

    Sample with bootstrap

---
+++ js/highlight_advance.js
@@ -0,0 +1,7 @@
+/*!
+ * Scrollspy Plugin
+ * Author: m00g
+ * Licensed under the MIT license
+ * Description: Hack for Twitter bootstrap's, hightlights the progression of the article.
+ * Uses bootstrap scrollspy
+ */
